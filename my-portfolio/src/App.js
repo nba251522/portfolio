276416a517e490ac701c16f4b1269b219c63a6d5
thomas <inquister12345678@gmail.com>
2023-12-09 01:00:09 -0800

    added code to the app.js so that the app is importing components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Navigation />
+
+        <main>
+          <Switch>
+            <Route path="/" exact component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

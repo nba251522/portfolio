c5e9886e5d221ff352ff5bafb97b9edfccc3a9c6
thomas <inquister12345678@gmail.com>
2023-12-10 18:37:31 -0800

    updated the Navigation file

---
+++ my-portfolio/src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">About Me</Link></li>
+        <li><Link to="/portfolio">Past Works</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;

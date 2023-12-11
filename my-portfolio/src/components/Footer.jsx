fbdf5f313b1f13ab2e5e1b2fbd5e2c4f949e5ab6
thomas <inquister12345678@gmail.com>
2023-12-10 22:35:35 -0800

    added footer

---
+++ my-portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/thomas-er-9b77321b9">LinkedIn</a></li>
+        <li><a href="https://github.com/nba251522">GitHub</a></li>
+      </ul>
+      <p>Email: inquister12345678@gmail.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
